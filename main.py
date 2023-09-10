a8399b0d30cc5fc77b4eb21c30e35c45e25e4215
Abhishek Biradar <abhishekbiradar0207@gmail.com>
2023-09-10 08:36:00 +0530

    Change the size of password

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
-    author = db.Column(db.String(50), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
@@ -55,8 +55,8 @@ class User(UserMixin, db.Model):
     __tablename__ = "user_data"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
 
 
 with app.app_context():
